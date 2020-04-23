58e41dbba469eabcd66ddfd4ffdf913264fb8dd0
Dai Otsuka <guy1nthecha1r2049@gmail.com>
2020-04-23 14:38:25 +0900

    add:カスタムユーザモデル作成機能及びスーパーユーザ作成機能を追加しました。(単体テストコード実装済み)

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'core']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
